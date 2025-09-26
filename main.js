7f49d08341f7d71f6dbe4fbc3f684af0b222b283
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-09-26 02:26:43 -0400

    Refactor to get game working in the DOM

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // Reference: https://thejsway.net/chapter09/
 
+// Set variables
+let currentTurn = document.querySelector("span");
+let result = document.querySelector("h3");
+
 class TicTacToe {
   constructor() {
     this.currentPlayer = "X";
@@ -11,28 +15,78 @@ class TicTacToe {
     ];
   }
 
-  playerMove(row, col) {
-    // add X or O to board at proper index
-    this.board[row][col] = this.currentPlayer;
+  newBoard() {
+    console.log("New Game is starting...");
+    const ul = document.querySelector("ul");
+
+    for (let i = 0; i < 9; i++) {
+      const li = document.createElement("li");
+      li.id = `sq-${i}`;
+
+      // Add event listeners to board
+      li.onclick = () => this.playerMove(i);
+      ul.appendChild(li);
+    }
+  }
+
+  playerMove(i) {
+    // Look up board index based on click
+    let indexMap = {
+      0: [0, 0],
+      1: [0, 1],
+      2: [0, 2],
+      3: [1, 0],
+      4: [1, 1],
+      5: [1, 2],
+      6: [2, 0],
+      7: [2, 1],
+      8: [2, 2],
+    };
+
+    // Add X or O to board based on player click
+    const square = document.getElementById(`sq-${i}`);
+    const row = indexMap[i][0];
+    const col = indexMap[i][1];
+
+    if (this.board[row][col] === null) {
+      square.innerText = this.currentPlayer;
+      square.onclick = null;
+      this.board[row][col] = this.currentPlayer;
+    }
+
+    // Check if current player won after each move
     this.checkWinner();
   }
 
   checkWinner() {
-    // check if current player won after each move
-    // what does winning mean?
-    // across: board[0][0] === board[0][1] === board[0][2]
-    // down: board[0][0] === board[1][0] === board[2][0]
-    // diagonal: board[0][0] === board[1][1] === board[2][2]
-    // other diagonal: board[0][2] === board[1][1] === board[2][0]
-
-    console.log("Checking for winner");
+    // Check rows
     if (
       (this.board[0][0] &&
         this.board[0][0] === this.board[0][1] &&
         this.board[0][1] === this.board[0][2]) ||
+      (this.board[1][0] &&
+        this.board[1][0] === this.board[1][1] &&
+        this.board[1][1] === this.board[1][2]) ||
+      (this.board[2][0] &&
+        this.board[2][0] === this.board[2][1] &&
+        this.board[2][1] === this.board[2][2])
+    ) {
+      this.announceWinner();
+    } else if (
+      // Check columns
       (this.board[0][0] &&
         this.board[0][0] === this.board[1][0] &&
         this.board[1][0] === this.board[2][0]) ||
+      (this.board[0][1] &&
+        this.board[0][1] === this.board[1][1] &&
+        this.board[1][1] === this.board[2][1]) ||
+      (this.board[0][2] &&
+        this.board[0][2] === this.board[1][2] &&
+        this.board[1][2] === this.board[2][2])
+    ) {
+      this.announceWinner();
+    } else if (
+      // Check diagonals
       (this.board[0][0] &&
         this.board[0][0] === this.board[1][1] &&
         this.board[1][1] === this.board[2][2]) ||
@@ -40,8 +94,7 @@ class TicTacToe {
         this.board[0][2] === this.board[1][1] &&
         this.board[1][1] === this.board[2][0])
     ) {
-      this.winner = this.currentPlayer;
-      console.log(`Player ${this.winner} has won!`);
+      this.announceWinner();
     } else {
       // if no winner, changeTurn()
       console.log("No winner yet");
@@ -50,18 +103,31 @@ class TicTacToe {
   }
 
   changeTurn() {
-    // after play, change to next player
-    console.log("Changing Turns");
-    this.currentPlayer === "X"
-      ? (this.currentPlayer = "O")
-      : (this.currentPlayer = "X");
+    // After play, change to next player
+    console.log("Changing turns");
+    if (this.currentPlayer === "X") {
+      this.currentPlayer = "O";
+      currentTurn.innerText = "O";
+    } else {
+      this.currentPlayer = "X";
+      currentTurn.innerText = "X";
+    }
+  }
+
+  announceWinner() {
+    this.winner = this.currentPlayer;
+    result.innerText = `Player ${this.winner} has won!`;
+    console.log(`Player ${this.winner} has won!`);
+
+    // Remove event listeners when game is over
+    document.querySelectorAll("li").forEach((li) => {
+      li.onclick = null;
+    });
   }
 }
 
 const game = new TicTacToe();
+game.newBoard();
 
-game.playerMove(0, 0);
-game.playerMove(0, 1);
-game.playerMove(1, 1);
-game.playerMove(1, 2);
-game.playerMove(2, 2); // player X wins
+// Allow game reset
+document.querySelector("p").onclick = () => game.newBoard();
